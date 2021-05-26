30383a01eb3118c3713a58bb25669f94510e6cd1
Manaze <manaze@yandex.ru>
2021-05-26 18:30:34 +0300

    DZ 2

--- src/ru/gb/dz/java2/kochemasov/lesson2/exception/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.dz.java2.kochemasov.lesson2.exception;
-
-public class MyArrayDataException extends NumberFormatException{
-    public MyArrayDataException() {
-        super("Массив должен содержать только цифры!");
-    }
-}
--- src/ru/gb/dz/java2/kochemasov/lesson2/exception/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.dz.java2.kochemasov.lesson2.exception;
-
-public class MyArraySizeException extends RuntimeException{
-    public MyArraySizeException() {
-        super("Массив должен быть строго размером 4х4!");
-    }
-}
