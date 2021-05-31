217d70cffa7049b8a7747fa2f3de95694fefada1
Manaze <manaze@yandex.ru>
2021-05-31 13:48:15 +0300

    DZ 2 fix

--- src/ru/gb/dz/java2/kochemasov/lesson2/massive/Massive.java
+++ src/ru/gb/dz/java2/kochemasov/lesson2/massive/Massive.java
@@ -5,18 +5,16 @@ import ru.gb.dz.java2.kochemasov.lesson2.exception.MyArraySizeException;
 
 public class Massive {
     public static final String[][] massive =
-            {{"1", "2", "-4", "2"}, {"3", "2", "0", "6"}};
-    //            {{"1", "2", "-4", "ы"},{"3", "2", "0", "6"}};
-    //            {{"1", "2", "0.2", "2"},{"3", "2", "0", "6"}};
-    //            {{"1", "2", "0,4", "2"},{"3", "2", "0", "6"}};
+            {{"1", "2", "-4", "2"}, {"3", "2", "0", "6"}, {"-3", "45", "2", "-6"}, {"23", "3", "0", "-6"}};
+    public static final int SIZE = 4;
 
     public static int checkMassive(String[][] massive) {
         int sum = 0;
-        if (massive.length != 2 || massive[0].length != 4) {
+        if (massive.length != SIZE || massive[0].length != SIZE) {
             throw new MyArraySizeException();
         } else {
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 4; j++) {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
                     if (massive[i][j].matches("(([-])?[0-9]+?)+")) {
                         sum += Integer.parseInt(massive[i][j]);
                     } else throw new MyArrayDataException();
